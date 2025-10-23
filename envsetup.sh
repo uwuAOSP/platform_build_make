71819cee010af22b3291ec42f245d0608601b5e0
Michael Bestas <mkbestas@lineageos.org>
2025-10-23 11:53:35 +0300

    envsetup: Build OOT kernel during lunch if needed
    Change-Id: Ie61ebb571dc758d1331d4fdec1f1456fd5bc5b32

--- envsetup.sh
+++ envsetup.sh
@@ -475,6 +475,11 @@ function _lunch_meat()
     local prebuilt_kernel=$(_get_build_var_cached TARGET_PREBUILT_KERNEL)
     if [[ "$no_kernel" == "true" ]] || [ -n "$prebuilt_kernel" ]; then
         unset INLINE_KERNEL_BUILDING
+        if [ -n "$(_get_build_var_cached TARGET_KERNEL_PLATFORM_SOURCE)" ]; then
+            if ! build_kernel; then
+                return 1
+            fi
+        fi
     else
         export INLINE_KERNEL_BUILDING=true
     fi
